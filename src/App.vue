0a277cb6c6ddad8553e7d724900f5d64d8bd112e
JuPeng <1662345805@qq.com>
2023-06-04 21:24:43 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+// https://github.com/vueuse/head
+useHead({
+  title: 'Ju Peng\'s Mini Site',
+  meta: [
+    { name: 'description', content: 'Ju Peng\'s Mini Site' },
+    {
+      name: 'theme-color',
+      content: () => isDark.value ? '#121213' : '#ffffff',
+    },
+  ],
+})
+</script>
